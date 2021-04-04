633a0bcc93d80c331ccdafcb164034710a7e3586
afraa <afraawael96@gmail.com>
2021-04-04 17:39:12 +0300

    new

---
+++ java/java.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let userName = prompt ('What is your name?')
+console.log (userName)
+let userAge = prompt ('What is your age?')
+console.log (userName)
+let userNationality = prompt ('Where are you from?')
+console.log (userNationality)
+let userPassion = prompt ('What is you passion?')
+console.log (userPassion)
+
+alert (' Your name is '  +  userName +  ' Your age is ' +  userAge +  ' You are from '  +  userNationality  +  ' And your passionate in '  +  userPassion);
